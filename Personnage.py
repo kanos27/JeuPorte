21c679c85f6471ae8d5ebcf01576914857ad872a
kanos27 <54512112+kanos27@users.noreply.github.com>
2019-12-03 14:14:32 +0100

    Create Personnage.py

---
+++ Personnage.py
@@ -0,0 +1,14 @@
+
+class Personnage():
+  # initiation avec mise en place de vie, ataque et peut-être inventaire
+  
+  # fonction donnant les infos importantes sur le personnage demandé
+  
+  # fonction permetant d'obtenir la vie du personnage
+  
+  # fonction permetant d'obtenir l'attaque du personnage
+  
+  # fonction d'attaque enlevant à la vie d'un personnage l'attaque d'un attaquant
+  
+  # fonction soignant le personnage grâce à sa vie et à la valeur du soin
+  
